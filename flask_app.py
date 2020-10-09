d20dee86ce9c2fb6676b50c99dd614b9b43d8bf4
Spectrix <owenhutch123@gmail.com>
2020-10-09 15:18:54 +0200

    Added better website directories

--- flask_app.py
+++ flask_app.py
@@ -4,7 +4,11 @@ import re
 app = Flask(__name__)
 
 @app.route('/')
-def bruh():
+def home():
+    return redirect("http://spectrix.technology/")
+
+@app.route('/botsaber')
+def botsaber():
     try:
         key = request.args.get('key')
         if any(char.isdigit() or char.isalpha() for char in key) and len(key) == 4:
@@ -12,7 +16,4 @@ def bruh():
         else:
             return "That doesn't seem to be a valid key..."
     except:
-        return """Welcome to my website.\n
-        You can put the following args at the end of the url: key (botsaber).\n
-        My actual website: https://spectrixofficial.github.io/.\n
-        Source code and website explanation here: https://github.com/SpectrixOfficial/spectrix.pythonanywhere.com"""
+        return "No args found. Something wrong? Check here: https://github.com/SpectrixOfficial/spectrix.pythonanywhere.com/"
